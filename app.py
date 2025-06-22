84e776fe3bbb0570cff8a1b6c6b867236890c29c
Chirag Singhal <2021bmm1001@iitrpr.ac.in>
2025-06-22 17:46:38 +0530

    Save local changes before pulling

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ def addition(a, b):
     """Returns the sum of a and b."""
     return a + b
 
-def subtraction(a, b, c):
+def subtraction(a, b):
     """Returns the difference of a and b."""
-    return a - b - c
+    return a - b
 
 def division(a, b):
     """Returns the quotient of a and b."""
