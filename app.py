875647c1961998f0de14e17796b39a47bf35ea3c
Chirag Singhal <101389606+chigs28@users.noreply.github.com>
2025-06-22 17:52:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ def addition(a, b, c):
     """Returns the sum of a and b."""
     return a + b + c
 
-def subtraction(a, b, c):
+def subtraction(a, b, e):
     """Returns the difference of a and b."""
-    return a - b - c
+    return a - b - e
 
 def division(a, b):
     """Returns the quotient of a and b."""
